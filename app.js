d152f7663aa20a451e5186061f3a9dbc7afa88a6
Vladyslava_Kurbanova <vladyslava.kurbanova@epam.com>
2016-09-02 13:21:43 +0300

    added ability to add document

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ var methodOverride = require('method-override');
 var mongoose       = require('mongoose');
 
 // configuration ===========================================
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 var db = require('./config/db');
+
 
 var port = 8080;
 
--- app/routes.js
+++ app/routes.js
@@ -10,4 +10,32 @@ module.exports = function(app) {
             res.json(documents);
         });
     });
+
+    app.post('/api/addDocument', function (req, res) {
+        // Get our form values. These rely on the "name" attributes
+        var DocTypeId = req.body.DocTypeId
+        var DocNumber = req.body.DocNumber
+        var PageCount = req.body.PageCount
+        var urlToImage = req.body.urlToImage
+        var CreationDate = req.body.CreationDate
+
+        // Submit to the DB
+        Document.collection.insertOne({
+            'DocTypeId': DocTypeId,
+            'DocNumber': DocNumber,
+            'PageCount': PageCount,
+            'urlToImage': urlToImage,
+            'CreationDate': CreationDate
+        }, function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send("There was a problem adding the information to the database.");
+            }
+            else {
+                console.log("Your document successfully added!!");
+                // And forward to success page
+                res.redirect("/api/documents");
+            }
+        });
+    });
 };
