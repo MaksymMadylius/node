8dd2f0f84a2ac7c23ec592848271790af94475d5
OlyaBoda <oliab1904@gmail.com>
2016-09-07 18:49:37 +0300

    Fixed failed merged config file.

---
+++ Scheduler.IntegrationTests/RequestManager/IRequester.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+
+namespace Scheduler.IntegrationTests.RequestManager
+{
+    interface IRequester
+    {
+        IEnumerable<Tuple<HttpResponseMessage, TimeSpan>> Get(string url, int usersCount, JArray jsonArray);
+        IEnumerable<Tuple<HttpResponseMessage, TimeSpan>> Post(string url, int usersCount, JArray jsonArray);
+    }
+}
